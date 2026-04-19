eb865458bec0c6eab88deb507a3bc4517a11b75e
SteveNerd22 <carmelo.spadaro.376@gmail.com>
2026-04-19 07:21:42 +0200

    aggiornata ux dropdown lingue

--- page.js
+++ page.js
@@ -1,17 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const langWrapper = document.querySelector('.dropdown');
     const langButton = document.getElementById('currentLang');
     const langOptions = document.querySelectorAll('#langOptions a');
+    const langMenu = document.getElementById('langOptions');
+
+    langButton.addEventListener('click', function(e) {
+        e.stopPropagation();
+        langMenu.classList.toggle('show');
+    });
 
     langOptions.forEach(option => {
         option.addEventListener('click', function(e) {
             e.preventDefault();
+
             const newLang = this.getAttribute('data-lang');
             const newFlag = this.getAttribute('data-flag');
-
             langButton.querySelector('.flag').textContent = newFlag;
             langButton.querySelector('.label').textContent = newLang;
 
-            console.log("Lingua selezionata:", newLang);
+            langMenu.classList.remove('show');
         });
     });
-})
+
+    document.addEventListener('click', function(e) {
+        if (!langWrapper.contains(e.target)) {
+            langMenu.classList.remove('show');
+        }
+    });
+});
